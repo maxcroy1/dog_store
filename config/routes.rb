6766d19569f3e8d0cba41f0a340e50ea00965b4d
maxcroy1 <max.croy1@gmail.com>
2020-07-07 09:59:35 -0500

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :items
-  resources :users
+  resources :orders, only: [:index, :show]
+  resources :items, only: [:index, :show]
+  resources :users, only: [:new, :create, :show, :edit, :update, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'application#home'
 end
