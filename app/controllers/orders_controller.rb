54957cb6ab45dc758d0308107ff942beec262983
maxcroy1 <max.croy1@gmail.com>
2020-07-09 12:16:25 -0500

    Who knows, lots o stuff, it works thank god

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,11 +6,16 @@ class OrdersController < ApplicationController
         @order.build_shipping_info
         @order.build_payment_info
         @order.build_billing_info
+        set_user
     end
 
     def create
-        @order = Order.create(order_params)
-        redirect_to order_path
+        @user = User.create(order_params[:user_attributes])
+        @shipping_info = ShippingInfo.create(order_params[:shipping_info_attributes].merge!(user_id: @user.id))
+        @billing_info = BillingInfo.create(order_params[:billing_info_attributes].merge!(user_id: @user.id))
+        @payment_info = PaymentInfo.create(order_params[:payment_info_attributes].merge!(user_id: @user.id, billing_info_id: @billing_info.id))
+        @order = Order.create(total: order_params[:total], user_id: @user.id, shipping_info_id: @shipping_info.id, billing_info_id: @billing_info.id, payment_info_id: @payment_info.id )
+        redirect_to order_path(@order)
     end
 
     def show
@@ -24,6 +29,7 @@ class OrdersController < ApplicationController
     end
 
     def order_params
-        params.require(:order).permit(:total, user_attributes: [:username, :password, :email], billing_info_attributes: [:user_id, :address_one, :address_two, :city, :state, :country, :zip_code], shipping_info_attributes: [:user_id, :address_one, :address_two, :city, :state, :country, :zip_code], payment_info_attributes: [:billing_info_id, :user_id, :name_on_card, :card_number, :expiration, :cvc, :zip_code])
+        params.require(:order).permit(:total, user_attributes: [:username, :password, :email], shipping_info_attributes: [:address_one, :address_two, :city, :state, :country, :zip_code], billing_info_attributes: [:address_one, :address_two, :city, :state, :country, :zip_code], payment_info_attributes: [:name_on_card, :card_number, :expiration, :cvc, :zip_code] )
     end
+    
 end
